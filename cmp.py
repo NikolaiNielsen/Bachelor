4bce5965cc2355940e51f72b2015166a55a3db1a
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2018-05-09 21:18:04 +0200

    small cleanup in cmp.py

--- cmp.py
+++ cmp.py
@@ -1,15 +1,3 @@
-"""
-Next:
-Reciprocal lattice and lattice planes
-Scattering
-- Parameters:
-  - Incident angle
-  - Incident wavelength of light
-  - Start with simple cubic, and 90 degree incident angle (specific case)
-Distortion of Fermi Surface:
-- Transform Hamiltonian to k-space. Transforms from a matrix to just a number
-"""
-
 import itertools
 
 import numpy as np
