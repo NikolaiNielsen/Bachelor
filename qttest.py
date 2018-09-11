fe8f46a48cc83563487c7e966a72d23423549b57
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2018-09-11 15:14:39 +0200

    try adding options window

--- qttest.py
+++ qttest.py
@@ -17,12 +17,16 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(self._main)
         layout = QtWidgets.QHBoxLayout(self._main)
 
-        static_fig, static_ax = Lattice(returns=True, plots=False)
-
-        static_canvas = FigureCanvas(static_fig)
+        # static_fig, static_ax = Lattice(returns=True, plots=False)
+        static_fic = plt.figure(figsize=(5, 3))
+        static_canvas = FigureCanvas(static_fic)
         layout.addWidget(static_canvas)
         self.addToolBar(NavigationToolbar(static_canvas, self))
-        static_ax.mouse_init()
+
+        self._static_ax = static_canvas.figure.subplots()
+        t = np.linspace(0, 10, 501)
+        self._static_ax.plot(t, np.tan(t), ".")
+        # static_ax.mouse_init()
 
         options = OptionsWindow()
         layout.addWidget(options)
@@ -31,10 +35,20 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 class OptionsWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.title = "Options window"
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 200
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
         button = QtWidgets.QPushButton("PyQt5 Button", self)
         button.setToolTip('this is an example button')
         button.move(100, 70)
-
         button.clicked.connect(self.on_click)
 
     # @pyqtSlot()
@@ -43,7 +57,6 @@ class OptionsWindow(QtWidgets.QWidget):
 
 
 if __name__ == "__main__":
-    Lattice()
     qapp = QtWidgets.QApplication(sys.argv)
     app = ApplicationWindow()
     app.show()
